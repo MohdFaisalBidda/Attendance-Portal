7f039e1285ad75061c76cfba70a868741796c87e
MohdFaisalBidda <biddafaisal@gmail.com>
2024-03-19 15:21:13 +0530

    Feat: login,register and reset page

--- frontend/src/components/auth/PasswordReset.jsx
+++ frontend/src/components/auth/PasswordReset.jsx
@@ -1,11 +1,96 @@
-import React from 'react'
+import React from "react";
+import { FaCalendarDays } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 function PasswordReset() {
   return (
-    <div>
-      PasswordReset
-    </div>
-  )
+    <section class="bg-gray-50 dark:bg-black h-screen">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+        <Link
+          to={"/login"}
+          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <FaCalendarDays className="w-8 h-8 mr-2" color="#db2777" />
+          Attendance Portal
+        </Link>
+        <div class="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
+          <h2 class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Change Password
+          </h2>
+          <form class="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
+            <div>
+              <label
+                for="email"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="name@company.com"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                for="password"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                New Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••••"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                for="confirm-password"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Confirm password
+              </label>
+              <input
+                type="confirm-password"
+                name="confirm-password"
+                id="confirm-password"
+                placeholder="••••••••"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <button
+              type="submit"
+              class="w-full text-white bg-[#db2777] hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Reset passwod
+            </button>
+          </form>
+          <div className="flex justify-between mt-5">
+            <Link
+              to={"/login"}
+              class="text-sm font-medium text-[#db2777] hover:underline dark:text-primary-500"
+            >
+              Login Here
+            </Link>
+            <Link
+              to={"/register"}
+              class="text-sm font-medium text-[#db2777] hover:underline dark:text-primary-500"
+            >
+              Register Here
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default PasswordReset
+export default PasswordReset;
