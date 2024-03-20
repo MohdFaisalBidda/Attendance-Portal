04e4893cf2ec93c28ab812e419fc63566aa3cef0
MohdFaisalBidda <biddafaisal@gmail.com>
2024-03-20 16:31:58 +0530

    Fix: some fix

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -103,7 +103,6 @@ function Dashboard() {
                         <>
                           <tr class="border-b dark:border-gray-700">
                             <td
-                              scope="row"
                               class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white text-xl"
                             >
                               {item.username}
